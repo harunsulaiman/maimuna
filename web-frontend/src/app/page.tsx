a6069a68e305ac0c1971676fe0e15938694b62ab
harun sulaiman <harun881@yahoo.com>
2026-09-18 11:06:15 +0200

    add

--- web-frontend/src/app/page.tsx
+++ web-frontend/src/app/page.tsx
@@ -161,7 +161,7 @@ export default function HomePage() {
 
     try {
       // Connect to your Python Flask backend
-      const response = await fetch("http://127.0.0.1:5000/api/detect", {
+      const response = await fetch("https://maimuna.onrender.com/api/detect", {
         method: "POST",
         body: formData,
       });
